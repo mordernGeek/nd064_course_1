b240da6b404cee9cc2db183fb5017120d3e8bb4a
mordern_geek <dyopal@gmail.com>
2021-11-05 11:24:30 +0100

    Techtrends on Docker
    Successfully see techtrends from 7111 dockerized container

--- exercises/go-helloworld/main.go
+++ exercises/go-helloworld/main.go
@@ -3,7 +3,7 @@ package main
 import (
     "fmt"
     "net/http"
-)
+) 
 
 func helloWorld(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Hello World")
--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,9 +1,9 @@
+from logging import Logger
 import sqlite3
+#from typing import get_args
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
-post_count = 0 
-db_connection_count = 0 
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
@@ -17,10 +17,7 @@ def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
-    title = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
     connection.close()
-    app.logger.info('Article "'+ title  +'" Retrieved')
     return post
 
 # Define the Flask application
@@ -31,28 +28,31 @@ app.config['SECRET_KEY'] = 'your secret key'
 @app.route('/')
 def index():
     connection = get_db_connection()
-    #db_connection_count = db_connection_count + 1 
     posts = connection.execute('SELECT * FROM posts').fetchall()
-    #post_count == len(posts)
     connection.close()
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
-def post(post_id):
+def post(post_id): 
+     
     post = get_post(post_id)
-    if post is None:
-      return render_template('404.html'), 404
-      app.logger.info('404 Error Status Returned')
-    else:
-      return render_template('post.html', post=post)
 
+    if post is None:
+        app.logger.warning('404 Not found error')
+        return render_template('404.html'), 404
+      
+    else:
+        app.logger.info('post was found')
+        return render_template('post.html', post=post)
+      
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us retrieved successfully')
     return render_template('about.html')
-    app.logger.info('About Us Page Retrieved Successfully')
+    
 
 # Define the post creation functionality 
 @app.route('/create', methods=('GET', 'POST'))
@@ -66,12 +66,26 @@ def create():
         else:
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content),
-                         app.logger.info('Article "'+ title  +'" Created'))
+                         (title, content))
+            #app.logger.info('New Article ' + title  + ' is created!')
             connection.commit()
             connection.close()
-
+            
             return redirect(url_for('index'))
+
+    else:
+        
+        title = request.form['title']
+        content = request.form['content']
+
+        connection = get_db_connection()
+        connection.execute('SELECT FROM posts (title) VALUES (?)',
+                         (title))
+            #app.logger.info('New Article ' + title  + ' is retrieved!')
+        connection.commit()
+        connection.close()
+
+        return redirect(url_for('index'))
 
     return render_template('create.html')
 
@@ -83,19 +97,26 @@ def healthz():  #def healthcheck():
         response=json.dumps({"result":"OK - healthy"}),
         status=200,
         mimetype='application/json'
-
+   
     )
     app.logger.info('Healthz endpoint request successful')
-
+    
     return response
+
+
 # adding the metrics endpoint
 @app.route('/metrics')
 def metrics():
+    counter = 0
+    connection = get_db_connection()
+    counter = counter + 1
+    posts = connection.execute('SELECT * FROM posts')
+
+    connection.close()
     response = app.response_class(
-        response=json.dumps({"status":"success","data":{"db_connection_count":db_connection_count,"post_count":post_count}}),
+        response=json.dumps({"status":"success","data":{"db_connection_count":counter,"post_count":posts}}),
         status=200,
         mimetype='application/json'
-
     )
     app.logger.info('Metrics request successful')
     return response
